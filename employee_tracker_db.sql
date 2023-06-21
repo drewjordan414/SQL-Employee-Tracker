1b956e6c8bd2800274f5500a63b5eee41c056ab5
Drew Jordan <ajordan12@luc.edu>
2023-06-20 21:56:34 -0500

    db updates

--- employee_tracker_db.sql
+++ employee_tracker_db.sql
@@ -1,6 +1,9 @@
-CREATE DATABASE IF NOT EXISTS employee_tracker_db;
-USE employee_tracker_db;
+-- DB CREATION 
+CREATE DATABASE employee_tracker_db;
 
+
+
+-- TABLE CREATION
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
@@ -27,3 +30,19 @@ CREATE TABLE employee (
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
+CREATE TABLE seed (
+    id INT NOT NULL AUTO_INCREMENT,
+    seed VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- INSERTING DEPARTMENTS
+INSERT INTO department (name)
+VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
+
+--INSERT ROLES
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1), ("Salesperson", 80000, 1), ("Lead Engineer", 150000, 2), ("Software Engineer", 120000, 2), ("Accountant", 125000, 3), ("Legal Team Lead", 250000, 4), ("Lawyer", 190000, 4);
+-- INSERT EMPLOYEES
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, NULL), ("Mike", "Chan", 2, 1), ("Ashley", "Rodriguez", 3, NULL), ("Kevin", "Tupik", 4, 3), ("Malia", "Brown", 5, NULL), ("Sarah", "Lourd", 6, 5), ("Tom", "Allen", 7, NULL);

b77421ebd37a0a15f6dabb091d105d37efdd5349
Drew Jordan <ajordan12@luc.edu>
2023-06-20 21:42:07 -0500

    finished adding functions for choices

--- index.js
+++ index.js
@@ -15,101 +15,140 @@ connection.connect(function (err) {
     start();
 });
 // start function 
-function start(){
+function start() {
     inquirer
-    .prompt({
-        name: "action",
-        type: "list",
-        message: "What would you like to do?",
-        choices:[
-            "View All departments",
-            "View All roles",
-            "View All employees",
-            "Add a department",
-            "Add a role",
-            "Add an employee",
-            "Update an employee role",
-            "Exit"
-        ]
-    })
-    .then(function(answer){
-        switch (answer.action){
-            case "View All departments":
-                viewAllDepartments();
-                break;
-            case "View All roles":
-                viewAllRoles();
-                break;
-            case "View All employees":
-                viewAllEmployees();
-                break;
-            case "Add a department":
-                addDepartment();
-                break;
-            case "Add a role":
-                addRole();
-                break;
-            case "Add an employee":
-                addEmployee();
-                break;
-            case "Update an employee role":
-                updateEmployeeRole();
-                break;
-            case "Exit":
-                connection.end();
-                break;
-        }
-    })
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View All departments",
+                "View All roles",
+                "View All employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update an employee role",
+                "Exit"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View All departments":
+                    viewAllDepartments();
+                    break;
+                case "View All roles":
+                    viewAllRoles();
+                    break;
+                case "View All employees":
+                    viewAllEmployees();
+                    break;
+                case "Add a department":
+                    addDepartment();
+                    break;
+                case "Add a role":
+                    addRole();
+                    break;
+                case "Add an employee":
+                    addEmployee();
+                    break;
+                case "Update an employee role":
+                    updateEmployeeRole();
+                    break;
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        })
 }
 // functions for each choice 
-function viewAllDepartments(){
-    connection.query("SELECT * FROM department", function(err, res){
+function viewAllDepartments() {
+    connection.query("SELECT * FROM department", function (err, res) {
         if (err) throw err;
         console.table(res);
         start();
     })
 };
-function viewAllRoles(){
-    connection.query("SELECT * FROM role", function(err, res){
+function viewAllRoles() {
+    connection.query("SELECT * FROM role", function (err, res) {
         if (err) throw err;
         console.table(res);
         start();
     })
 };
-function viewAllEmployees(){
-    connection.query("SELECT * FROM employee", function(err, res){
+function viewAllEmployees() {
+    connection.query("SELECT * FROM employee", function (err, res) {
         if (err) throw err;
         console.table(res);
         start();
     })
 };
-function addDepartment(){
+function addDepartment() {
     inquirer
-    .prompt({
-        name: "department",
-        type: "input",
-        message: "Please enter the name of the department you would like to add."
-    })
-    .then(function(answer){
-        connection.query("INSERT INTO department SET ?", {name: answer.department}, function(err, res){
-            if (err) throw err;
-            console.log("Department added successfully!");
-            start();
+        .prompt({
+            name: "department",
+            type: "input",
+            message: "Please enter the name of the department you would like to add."
         })
-    })
+        .then(function (answer) {
+            connection.query("INSERT INTO department SET ?", { name: answer.department }, function (err, res) {
+                if (err) throw err;
+                console.log("Department added successfully!");
+                start();
+            })
+        })
 };
-function addRole(){
+function addRole() {
     inquirer
-    .prompt({
-        name: "title",
-        type: "input",
-        message: "Please enter the title of the role you would like to add."
-    })
-    .then(function(answer){
-        connection.query("INSERT INTO role SET ?", {title: answer.title}, function(err, res){
-            if (err) throw err;
-            console.log("Role added successfully!");
-            start();
+        .prompt({
+            name: "title",
+            type: "input",
+            message: "Please enter the title of the role you would like to add."
         })
-    })
+        .then(function (answer) {
+            connection.query("INSERT INTO role SET ?", { title: answer.title }, function (err, res) {
+                if (err) throw err;
+                console.log("Role added successfully!");
+                start();
+            })
+        })
 };
+function addEmployee() {
+    inquirer
+        .prompt({
+            name: "first_name",
+            type: "input",
+            message: "Please enter the first name of the employee you would like to add."
+        })
+        .then(function (answer) {
+            connection.query("INSERT INTO employee SET ?", { first_name: answer.first_name }, function (err, res) {
+                if (err) throw err;
+                console.log("Employee added successfully!");
+                start();
+            })
+        })
+}
+function updateEmployeeRole() {
+    inquirer
+        .prompt([
+            {
+                name: 'employeeId',
+                type: 'list',
+                message: 'Which employee\'s role do you want to update?',
+                choices: employeeChoices,
+            },
+            {
+                name: 'roleId',
+                type: 'list',
+                message: 'Which role do you want to assign to the selected employee?',
+                choices: roleChoices,
+            },
+        ])
+        .then(function (answer) {
+            connection.query('UPDATE employees SET ? WHERE ?', [{ role_id: answer.roleId }, { id: answer.employeeId }], function (err) {
+                if (err) throw err;
+                console.log('Updated employee\'s role successfully!');
+                start();
+            });
+        });
+}
